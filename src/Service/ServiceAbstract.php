eb7cc263161355c2f027b30478aeeb881ca57855
Mostafa Soufi <mostafa.soufi@hotmail.com>
2020-04-20 14:26:10 +0300

    Set timeout

--- src/Service/ServiceAbstract.php
+++ src/Service/ServiceAbstract.php
@@ -30,7 +30,7 @@ abstract class ServiceAbstract
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_TIMEOUT => 5,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
